6f31dace41844b5b7ed8e813e8fe94c29603b9da
Beka Batmanashvili <Bekabatmanashvili1@gmail.com>
2022-02-15 01:38:31 +0400

    chore: rebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
 /* eslint-disable jsx-a11y/heading-has-content */
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { object, string } from "yup";
+import { useFormik } from "formik";
+import InputMask from "react-input-mask";
+import emailjs from "@emailjs/browser";
 import "./App.css";
 
+export const numberRegex = /[0-9]+$/gi;
+
+const userSchema = object({
+  firstname: string()
+    .required("აუცილებელი ველი")
+    .min(3, "მინიმუმ 3 სიმბოლო")
+    .max(20, "მაქსიმუმ 20 სიმბოლო"),
+  lastname: string()
+    .required("აუცილებელი ველი")
+    .min(3, "მინიმუმ 3 სიმბოლო")
+    .max(40, "მაქსიმუმ 40 სიმბოლო"),
+  phone: string()
+    .required("აუცილებელი ველი")
+    .matches(numberRegex, {
+      message: "შეიყვანე სწორი მობილურის ნომერი",
+      excludeEmptyString: true,
+    })
+    .min(9, "შეიყვანე მინიმუმ 9 სიმბოლო"),
+  email: string().required("აუცილებელი ველი").email("შეიყვანე სწორი მეილი"),
+  region: string().min(3, "მიმუმ 3 სიმბოლო").max(40, "მაქსიმუმ 40 სიმბოლო"),
+});
+
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      firstname: "",
+      lastname: "",
+      phone: "",
+      email: "",
+      region: "",
+    },
+    validationSchema: userSchema,
+    onSubmit: (values) => {
+      emailjs
+        .send(
+          process.env.REACT_APP_SERVICE_ID,
+          process.env.REACT_APP_TEMPLATE_ID,
+          values,
+          process.env.REACT_APP_USER_ID
+        )
+        .then(function (response) {
+          alert("დიდი მადლობა! დაგიკავშირდებით უახლოეს მომავალში.");
+          // console.log("SUCCESS!", response.status);
+        })
+        .catch((err) => {
+          alert(
+            "დროებით შეფერხებაა, გთხოვთ დაგვიკავშირდეთ სოციალური ქსელის საშუალებით."
+          );
+          // console.warn("FAILED...", error);
+        });
+    },
+  });
+
   return (
     <app-root _nghost-ffy-c17="" ng-version="12.2.12">
       <app-header _ngcontent-ffy-c17="" _nghost-ffy-c6="">
@@ -279,7 +335,7 @@ function App() {
       <app-register _ngcontent-ffy-c17="" _nghost-ffy-c8="">
         <main _ngcontent-ffy-c8="" id="register1" className="register">
           <div _ngcontent-ffy-c8="" className="formholder">
-            <form _ngcontent-ffy-c8="">
+            <form _ngcontent-ffy-c8="" onSubmit={formik.handleSubmit}>
               <h4
                 _ngcontent-ffy-c8=""
                 style={{ color: "#495057", textAlign: "center" }}
@@ -302,6 +358,8 @@ function App() {
                     _ngcontent-ffy-c8=""
                     type="text"
                     id="firstname"
+                    value={formik.values.firstname}
+                    onChange={formik.handleChange}
                     placeholder="სახელი"
                     className="form-control"
                     style={{
@@ -317,6 +375,17 @@ function App() {
                   />
                 </div>
               </div>
+              {formik.touched.firstname && formik.errors.firstname ? (
+                <div
+                  style={{
+                    color: "red",
+                    marginLeft: "6%",
+                    marginBottom: 20,
+                  }}
+                >
+                  {formik.errors.firstname}
+                </div>
+              ) : null}
               <div _ngcontent-ffy-c8="" className="form-group">
                 <div _ngcontent-ffy-c8="" className="input-group">
                   <div _ngcontent-ffy-c8="" className="input-group-addon">
@@ -330,11 +399,21 @@ function App() {
                     _ngcontent-ffy-c8=""
                     type="text"
                     id="lastname"
+                    value={formik.values.lastname}
+                    onChange={formik.handleChange}
                     placeholder="გვარი"
                     className="form-control"
                   />
                 </div>
               </div>
+
+              {formik.touched.lastname && formik.errors.lastname ? (
+                <div
+                  style={{ color: "red", marginLeft: "6%", marginBottom: 20 }}
+                >
+                  {formik.errors.lastname}
+                </div>
+              ) : null}
               <div _ngcontent-ffy-c8="" className="form-group">
                 <div _ngcontent-ffy-c8="" className="input-group">
                   <div _ngcontent-ffy-c8="" className="input-group-addon">
@@ -344,15 +423,32 @@ function App() {
                       className="glyphicon glyphicon-earphone"
                     ></span>
                   </div>
-                  <input
-                    _ngcontent-ffy-c8=""
-                    type="text"
-                    id="phone"
-                    placeholder="ტელეფონის ნომერი"
-                    className="form-control"
-                  />
+                  <InputMask
+                    mask="(+\9\95)99-999-999"
+                    alwaysShowMask
+                    value={formik.values.phone}
+                    onChange={formik.handleChange}
+                  >
+                    {(inputProps) => (
+                      <input
+                        {...inputProps}
+                        _ngcontent-ffy-c8=""
+                        type="text"
+                        id="phone"
+                        placeholder="ტელეფონის ნომერი"
+                        className="form-control"
+                      />
+                    )}
+                  </InputMask>
                 </div>
               </div>
+              {formik.touched.phone && formik.errors.phone ? (
+                <div
+                  style={{ color: "red", marginLeft: "6%", marginBottom: 20 }}
+                >
+                  {formik.errors.phone}
+                </div>
+              ) : null}
               <div _ngcontent-ffy-c8="" className="form-group">
                 <div _ngcontent-ffy-c8="" className="input-group">
                   <div _ngcontent-ffy-c8="" className="input-group-addon">
@@ -366,11 +462,20 @@ function App() {
                     _ngcontent-ffy-c8=""
                     type="text"
                     id="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
                     placeholder="ელ. ფოსტა"
                     className="form-control"
                   />
                 </div>
               </div>
+              {formik.touched.email && formik.errors.email ? (
+                <div
+                  style={{ color: "red", marginLeft: "6%", marginBottom: 20 }}
+                >
+                  {formik.errors.email}
+                </div>
+              ) : null}
               <div _ngcontent-ffy-c8="" className="form-group">
                 <div _ngcontent-ffy-c8="" className="input-group">
                   <div _ngcontent-ffy-c8="" className="input-group-addon">
@@ -384,6 +489,8 @@ function App() {
                     _ngcontent-ffy-c8=""
                     type="text"
                     id="region"
+                    value={formik.values.region}
+                    onChange={formik.handleChange}
                     placeholder="რეგიონი"
                     className="form-control"
                   />
@@ -406,7 +513,7 @@ function App() {
                   _ngcontent-ffy-c8=""
                   type="button"
                   id="submitForm"
-                  onClick={() => alert("pressed")}
+                  onClick={formik.handleSubmit}
                   className="btn btn-default btn-lg btn-block button"
                 >
                   გაგზავნა
@@ -484,7 +591,7 @@ function App() {
                     alt=""
                     style={{
                       border: "1px solid #9d24ac",
-                      "border-radius": "15px 0",
+                      borderRadius: "15px 0",
                     }}
                   />
                 </div>
